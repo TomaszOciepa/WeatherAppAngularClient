19a9d640cd80c5ebc483fdc139bf9b8a370bd2a5
tomaszociepa <tom.ociepa@gmail.com>
2020-01-26 13:19:56 +0100

    bug fix

--- src/app/client/weather-api.service.ts
+++ src/app/client/weather-api.service.ts
@@ -11,7 +11,7 @@ export class WeatherApiService {
   token = "5edc3ee7e769f70237953bb6d7580423"
 
   getCurrentWeather(city){
-      return this.http.get<CurrentWeather>("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID="+this.token+"&lang=pl")
+      return this.http.get<CurrentWeather>("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID="+this.token+"&lang=pl")
       
   }
 
