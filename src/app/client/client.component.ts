a79a1c66800fa7c19cc8af327330f429c1c9ef60
tomszociepa <tom.ociepa@gmail.com>
2025-05-19 16:58:32 +0200

    change delete icon

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,60 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from 'ngx-webstorage';
-
+import { Component, OnInit } from "@angular/core";
+import { LocalStorageService } from "ngx-webstorage";
 
 @Component({
-  selector: 'client',
-  templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+  selector: "client",
+  templateUrl: "./client.component.html",
+  styleUrls: ["./client.component.css"],
 })
 export class ClientComponent implements OnInit {
+  addFavoritCity;
 
-  
-  addFavoritCity
-
-  addMyFavoritCity(c){
+  addMyFavoritCity(c) {
     this.addFavoritCity = c;
   }
 
-  star
+  star;
 
-  clock: number
-  selectedCity: string
+  clock: number;
+  selectedCity: string;
 
-  setStar(s){
-    this.star = s
+  setStar(s) {
+    this.star = s;
   }
 
-  getSelectedCity(city){
+  getSelectedCity(city) {
     this.selectedCity = city;
   }
 
-  getTime(){
-    return Date.now()
+  getTime() {
+    return Date.now();
   }
 
-  setTime(){
-    setInterval(()=>{
-      this.clock = this.getTime()
-    }, 1000)
+  setTime() {
+    setInterval(() => {
+      this.clock = this.getTime();
+    }, 1000);
   }
 
-  constructor(private localStorage: LocalStorageService) { }
+  constructor(private localStorage: LocalStorageService) {}
 
   ngOnInit() {
-
     // init
-    var myTab = []
-    myTab = this.localStorage.retrieve('cities')
-    if(myTab !== null && myTab.length >= 1){
-      this.getSelectedCity(myTab[0])    
-    } else(
-      this.getSelectedCity("Warszawa")
-    )
-      
-    
-  
-  this.setTime()
-  }
+    var myTab = [];
+    myTab = this.localStorage.retrieve("cities");
+    if (myTab !== null && myTab.length >= 1) {
+      this.getSelectedCity(myTab[0]);
+    } else this.getSelectedCity("Warszawa");
 
+    this.setTime();
+  }
 }
--- src/app/client/weather-card/weather-card.component.ts
+++ src/app/client/weather-card/weather-card.component.ts
@@ -1,71 +1,73 @@
-import { Component, OnInit, Input, Optional, Output, EventEmitter } from '@angular/core';
-import { WeatherApiService } from '../weather-api.service';
-
-
+import {
+  Component,
+  OnInit,
+  Input,
+  Optional,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { WeatherApiService } from "../weather-api.service";
 
 @Component({
-  selector: 'weather-card',
-  templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.css']
+  selector: "weather-card",
+  templateUrl: "./weather-card.component.html",
+  styleUrls: ["./weather-card.component.css"],
 })
 export class WeatherCardComponent implements OnInit {
-  
-  @Input('star')
-  favorite
+  @Input("star")
+  favorite;
 
-  // @Output('deleteFavoriteCity')
+  // @Output("deleteFavoriteCity")
   // emiterDeleteFavoriteCity = new EventEmitter();
 
-  // deleteFavoriteCity(city){
-  //   this.emiterDeleteFavoriteCity.emit(city);
+  // deleteFavoriteCity() {
+  //   this.emiterDeleteFavoriteCity.emit(this.name);
   // }
 
-  @Output('addMyFavoritCity')
+  @Output("addMyFavoritCity")
   emiterSetFavoriteCity = new EventEmitter();
 
-  setFavoriteCity(){
+  setFavoriteCity() {
     this.emiterSetFavoriteCity.emit(this.name);
-  }  
-
-
-  name
-  date
-  wind
-  clouds
-  humidity
-  pressure
-  temp
-  temp_min
-  temp_max
-  sunrise
-  sunset
-  description
-  icon
-
-  @Input('selectedCity')
-  set setCurrentWeather(city){
-      this.http.getCurrentWeather(city).subscribe(response =>{
-          this.name = city;
-          this.date = Date.now();
-          this.wind = response.wind.speed;
-          this.clouds = response.clouds.all;
-          this.humidity = response.main.humidity;
-          this.pressure = response.main.pressure;
-          this.temp = Math.round(response.main.temp);
-          this.temp_min = Math.round(response.main.temp_min);
-          this.temp_max = Math.round(response.main.temp_max);
-          this.sunrise = response.sys.sunrise;
-          this.sunset = response.sys.sunset;
-          this.description = response.weather[0].description;
-          this.icon = "http://openweathermap.org/img/wn/"+response.weather[0].icon+"@2x.png";
-      })
-      
   }
 
-    
-  constructor(private http:WeatherApiService) { }
+  name;
+  date;
+  wind;
+  clouds;
+  humidity;
+  pressure;
+  temp;
+  temp_min;
+  temp_max;
+  sunrise;
+  sunset;
+  description;
+  icon;
 
-  ngOnInit() {
+  @Input("selectedCity")
+  set setCurrentWeather(city) {
+    this.http.getCurrentWeather(city).subscribe((response) => {
+      this.name = city;
+      this.date = Date.now();
+      this.wind = response.wind.speed;
+      this.clouds = response.clouds.all;
+      this.humidity = response.main.humidity;
+      this.pressure = response.main.pressure;
+      this.temp = Math.round(response.main.temp);
+      this.temp_min = Math.round(response.main.temp_min);
+      this.temp_max = Math.round(response.main.temp_max);
+      this.sunrise = response.sys.sunrise;
+      this.sunset = response.sys.sunset;
+      this.description = response.weather[0].description;
+      this.icon =
+        "http://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@2x.png";
+    });
   }
 
+  constructor(private http: WeatherApiService) {}
+
+  ngOnInit() {}
 }
