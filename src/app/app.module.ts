2b54fd9859db3b71507322869365a265b6decb35
tomszociepa <tom.ociepa@gmail.com>
2025-05-19 15:12:44 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ClientModule } from './client/client.module';
-import { HttpClientModule } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localePl from '@angular/common/locales/pl';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ClientModule } from "./client/client.module";
+import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import localePl from "@angular/common/locales/pl";
+import { MatIconModule } from "@angular/material";
 
-registerLocaleData(localePl, 'pl');
+registerLocaleData(localePl, "pl");
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ClientModule,
     HttpClientModule,
+    MatIconModule,
   ],
-  
-  providers: [
-  
-  ],
-  bootstrap: [AppComponent]
-})
-export class AppModule { 
 
-  
-}
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
